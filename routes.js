eec5029487914f8d2122104f885d14c0f9bb51aa
Paulo Lopes <silvavitorlopes711@gmail.com>
2020-05-14 18:27:41 -0300

    chore: routes.js

---
+++ routes.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const routes = express.Router()
+
+
+const recipes = require('./recipes-data')
+const homeRecipes = recipes.slice(0,6)
+
+const aboutdata = require('./about-data')
+
+
+routes.get('/', function(req, res){
+
+    const homeData = {
+        title: 'As melhores receitas',
+        description: 'Aprenda a construir os melhores pratos com receitas criadas por profissionais do mundo inteiro.',
+        image: '/assets/chef.png'
+    }
+    
+
+    return res.render('home', {homeRecipes: homeRecipes, homeData})
+ })
+
+routes.get('/about', function(req, res){
+    return res.render('about', {aboutdata})
+})
+
+routes.get('/recipes', function(req, res){
+    return res.render('recipes', {recipes})
+})
+
+routes.get('/recipes/:id', function(req, res){
+    const id = req.params.id;
+
+    const recipe = recipes.find( recipe => {
+        return recipe.id == id;
+    })
+
+    if(!recipe) {
+        res.status(404).render('not-found')
+    }
+
+    res.render('recipe', {recipe})
+})
+
+routes.use(function(req, res){
+    res.status(404).render('not-found')
+})
+
+
+module.exports = routes
--- server.js
+++ server.js
@@ -1,58 +1,18 @@
 const express = require('express')
 const nunjucks = require('Nunjucks')
-
-const recipes = require('./recipes-data')
-const homeRecipes = recipes.slice(0,6)
-
-const aboutdata = require('./about-data')
+const routes = require('./routes')
 
 const server = express()
 server.set("view engine", "njk")
 server.use(express.static('public'))
+server.use(routes)
 
 nunjucks.configure("views", {
     autoescape:false,
     express:server
 })
 
- //Routers
-server.get('/', function(req, res){
-
-    const homeData = {
-        title: 'As melhores receitas',
-        description: 'Aprenda a construir os melhores pratos com receitas criadas por profissionais do mundo inteiro.',
-        image: '/assets/chef.png'
-    }
-    
-
-    return res.render('home', {homeRecipes: homeRecipes, homeData})
- })
-
-server.get('/about', function(req, res){
-    return res.render('about', {aboutdata})
-})
-
-server.get('/recipes', function(req, res){
-    return res.render('recipes', {recipes})
-})
-
-server.get('/recipes/:id', function(req, res){
-    const id = req.params.id;
-
-    const recipe = recipes.find( recipe => {
-        return recipe.id == id;
-    })
-
-    if(!recipe) {
-        res.status(404).render('not-found')
-    }
-
-    res.render('recipe', {recipe})
-})
-
-server.use(function(req, res){
-    res.status(404).render('not-found')
-})
+ //Ro
 
 server.listen(5000, function(){
     console.log('Server is running.')
